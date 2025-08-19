c0828f9966a5ade982f675d8b89ba71fcef2611f
kaspe <kasperi.huttunen@metropolia.fi>
2025-08-19 14:37:47 +0300

    initial commit

---
+++ mod01/01.py
@@ -0,0 +1,12 @@
+#kommentti
+print('hello\nworld')
+print('yeet')
+print(1)
+
+#yksinkertainen muttuja
+nimi = 'jorma'
+print(nimi)
+
+#syöte, eli tietoa käyttäjältä
+nimi2 = input('Hei,anna nimesi: ')
+print(nimi2)
